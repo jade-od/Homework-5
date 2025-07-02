345987d6670e4db59f788792f84f462b7d1ccc70
jade-od <jade.od.04@gmail.com>
2025-07-03 01:32:11 +0900

    testing

--- pyFiles/MAINwEDIT.py
+++ pyFiles/MAINwEDIT.py
@@ -539,7 +539,7 @@ class Ui_MainWindow(object):
 
     def connect(self):
         self.cnx = mysql.connector.connect(user='root',
-                                               password='123456789',
+                                               password='',
                                                host='127.0.0.1',
                                                database='homework04')
         
